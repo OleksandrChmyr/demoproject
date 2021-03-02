0820f8c348340d43e97c8c18cbde8a50699fcd2c
OleksandrChmyr <alexanderch87@gmail.com>
2021-03-02 21:42:01 +0200

    added HT1

---
+++ src/main/java/com/example/demoproject/DemoprojectApplication.java
@@ -0,0 +1,15 @@
+package com.example.demoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootApplication
+public class DemoprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoprojectApplication.class, args);
+    }
+
+}
